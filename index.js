b8f419fe848b050451fcadfb2cc378f6c7afa885
Nimra-Zahoor <nimra.zahoor120153@gmail.com>
2023-06-26 13:26:22 +0500

    Text area added for car details added

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//Implement flow of a factory pattern for the creation of a car, 
-//in which there are different objects like seats, engine, doors,
-// multimedia, suspension, electrical system etc. Ensuring only a single 
-//object of the Car Factory is ever available, keep track of cars produced by 
-//the factory and provide an interface to get a list of all those cars and their counts.
-//Factory should help make a car with custom requirements 
-//for all mentioned components, with sensible defaults set.
+class CarFactory{
+  
+}
